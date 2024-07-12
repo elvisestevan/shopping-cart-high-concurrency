1047a6fc399036949f7218707a084157b9afc2c9
Elvis Estevan <elvis.estevan65@gmail.com>
2024-07-11 21:07:15 -0300

    spring boot web working

---
+++ src/main/kotlin/com/elvisestevan/shoppingcart/ShoppingCartHighConcurrencyApplication.kt
@@ -0,0 +1,11 @@
+package com.elvisestevan.shoppingcart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingCartHighConcurrencyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppingCartHighConcurrencyApplication>(*args)
+}
